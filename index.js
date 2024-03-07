185d4875c9e90ac38e99229125c5e172fd872575
euwilliamsantos <euwilliam.77@gmail.com>
2024-03-07 02:16:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var Dados, 
+
+Dados = prompt ('Olá Digite Acessar')
+
+
+confirm(' Você confirma acessar  o Formulario Digite ok  ')
+
+alert('Acesso realizado para prencher o formulario abaixo:  ' + Dados)
